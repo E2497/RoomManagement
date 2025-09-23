582405005a2fefff81162d85a5a4bcbf4c9c882a
Esraa Salah <esraasalah833@gmail.com>
2025-09-23 20:57:00 +0300

    Hom work select room

---
+++ RoomManagement/RevitCommands/Commands/RoomSelectionFilter.cs
@@ -0,0 +1,23 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI.Selection;
+
+namespace RoomManagement.RevitCommands.Commands
+{
+    internal class RoomSelectionFilter : ISelectionFilter
+    {
+        public bool AllowElement(Element elem)
+        {
+            if (elem.Category.Name == "Rooms")
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool AllowReference(Reference reference, XYZ position)
+        {
+            return false;
+
+        }
+    }
+}
--- RoomManagement/RevitCommands/Commands/RoomToFilledRegion.cs
+++ RoomManagement/RevitCommands/Commands/RoomToFilledRegion.cs
@@ -3,10 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Reflection;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI.Selection;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.Attributes;
 
 namespace RoomManagement.RevitCommands.Commands
 {
-    internal class RoomToFilledRegion
+    [TransactionAttribute(TransactionMode.Manual)]
+    public class RoomToFilledRegion : IExternalCommand
     {
+        public static UIDocument uidoc { get; set; }
+        public static Document doc { get; set; }
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            uidoc = commandData.Application.ActiveUIDocument;
+            doc = uidoc.Document;
+
+            FilteredElementCollector collector = new FilteredElementCollector(doc);
+            ISelectionFilter RoomFilter = new RoomSelectionFilter();
+
+            Reference sel = uidoc.Selection.PickObject(ObjectType.Element, RoomFilter);
+
+
+            throw new NotImplementedException();
+        }
     }
 }
